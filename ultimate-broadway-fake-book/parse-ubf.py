6645dd7a44f7323b04849ca8d841422a5ba3fa22
Niklas Morberg <niklas.morberg@gmail.com>
2021-02-01 09:27:38 +0100

    remove old code

--- ultimate-broadway-fake-book/parse-ubf.py
+++ ultimate-broadway-fake-book/parse-ubf.py
@@ -8,20 +8,13 @@ for i in range(1, 4):
         raw += data["document"]["text"]
 
 # %%
-songs = []
 with open("ubf-raw.csv", "w") as f:
-    for line in raw.split("\n"):
-        song = line.split(".")
-        # page, "song title"
-        f.write(f"{song[0]},\"{' '.join(song[1:])}\"\n")
-
-# %%
-for line in raw.splitlines():
-    index_entry = line.split(".", maxsplit=1)
-    page = index_entry[0].strip("_ ")
-    try:
-        song = index_entry[1].strip(". ")
-    except IndexError:
-        song = ""
-    print(f'{page},"{song}"')
+    for line in raw.splitlines():
+        index_entry = line.split(".", maxsplit=1)
+        page = index_entry[0].strip("_ ")
+        try:
+            song = index_entry[1].strip(". ")
+        except IndexError:
+            song = ""
+        f.write(f'{page},"{song}"\n')
 # %%
